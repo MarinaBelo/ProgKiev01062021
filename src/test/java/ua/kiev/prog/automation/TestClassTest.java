98fa027d0ffb5c4b5856b41599fb9dd5f5dc1fa5
MarinaBelo <martsipen@gmail.com>
2021-06-24 20:29:21 +0300

    Added first Test

---
+++ src/test/java/ua/kiev/prog/automation/TestClassTest.java
@@ -0,0 +1,13 @@
+package ua.kiev.prog.automation;
+
+import org.testng.annotations.Test;
+
+public class TestClassTest {
+    @Test
+    public void testMethod(){
+        System.out.println("Hi");
+    }
+
+
+
+}
