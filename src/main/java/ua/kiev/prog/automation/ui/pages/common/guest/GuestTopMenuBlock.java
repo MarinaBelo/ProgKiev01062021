939f95dc56e25a15471681296fe7d11e6d44cb30
MarinaBelo <martsipen@gmail.com>
2021-06-27 17:59:32 +0300

    Adding a SingletonPattern+Separated site logic for guest/registered user

--- src/main/java/ua/kiev/prog/automation/ui/pages/common/guest/GuestTopMenuBlock.java
+++ src/main/java/ua/kiev/prog/automation/ui/pages/common/guest/GuestTopMenuBlock.java
@@ -5,18 +5,18 @@ import org.openqa.selenium.WebElement;
 import ua.kiev.prog.automation.ui.pages.base.TopMenuBlock;
 
 public class GuestTopMenuBlock extends TopMenuBlock {
-    private boolean isAccountULDisplayed(){
+    private boolean isAccountUIDisplayed(){
         return this.account().findElement(By.xpath("./following-sibling::ul")).isDisplayed();
     }
 
     public WebElement accountRegistration(){                                                //Registration
-        if(!isAccountULDisplayed())
+        if(!isAccountUIDisplayed())
             this.account().click();
         return this.account().findElement(By.xpath("./following-sibling::ul/li[1]/a"));
     }
 
     public WebElement accountAuthorization(){                                               //Authorization
-        if(!isAccountULDisplayed())
+        if(!isAccountUIDisplayed())
             this.account().click();
         return this.account().findElement(By.xpath("./following-sibling::ul/li[2]/a"));
     }
